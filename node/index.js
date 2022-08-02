aad4a3de9d8808baafb56847736bbb588504062d
Sagar <dev.sagartmg@gmail.com>
2022-08-02 17:16:59 +0545

    init node

---
+++ node/index.js
@@ -0,0 +1,42 @@
+
+// Buffer and stream 
+
+
+// console.log(process.argv)
+// proces.exit()
+// process.env
+
+// console.log(__filename)
+// console.log(__dirname)
+
+
+// Modules
+// - core moudle
+// - local module
+// - third party module
+
+
+// Module system
+    // - commonJs
+    // - Es6 
+
+
+const fs = require("fs")
+
+// fs.writeFileSync("custom.txt","custom text")
+// let file_data = fs.readFileSync("custom.txt").toString();
+
+// fs.readFile("custom.txt",(err,data) => {
+//     console.log(data.toString())
+// });
+
+
+console.log(222);
+// console.log({file_data})
+
+
+
+
+
+
+
