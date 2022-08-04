ef8711a19e0d7da6c9238b3f752b140768d3d09b
Sagar <dev.sagartmg@gmail.com>
2022-08-04 17:00:31 +0545

    crud in node

--- node/index.js
+++ node/index.js
@@ -50,14 +50,79 @@ const dbconnect = require("./dbconnection")
 // console.log({dbconnect})
 
 
+// 1 => information
+// 2 => 200 success 
+// 3 => 304 redirect 
+// 4 => 400 401 403   client side error
+// 5 => 500 server side error
+
+
 
 const http = require("http")
 
 const server = http.createServer((req, res) => {
     // req.method
-    if (req.url == "/products") {
-        res.write("products list")
+    if (req.url == "/index.js") {
+        res.write(fs.readFileSync("./index copy.js"))
         res.end();
+    }
+
+    if (req.url == "/products") {
+
+        if (req.method == "GET") {
+            try {
+                // let a = b + c;
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                // res.write(fs.readFileSync("./products.json"))
+                res.write(fs.readFileSync("./index.html"))
+                res.end();
+            }
+            catch (err) {
+                res.writeHead(500);
+                res.write(JSON.stringify({ a: 1 }))
+                res.end();
+            }
+        } else if (req.method == "POST") {
+            console.log("1212")
+            req.on('data', function (data) {
+                let obj = JSON.parse(fs.readFileSync("./products.json"));
+                let products_arr = obj.products
+
+                products_arr.push(JSON.parse(data));
+                fs.writeFileSync("products.json", JSON.stringify({ products: products_arr }))
+            })
+            //fs.writeFileSync("file.txt", data)
+            req.on("end", function () {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end("success")
+            })
+        } else if (req.method == "PUT") {
+            req.on('data', function (data) {
+                let obj = JSON.parse(fs.readFileSync("./products.json"));
+                let products_arr = obj.products
+
+                let req_data = JSON.parse(data)
+
+                // let new_arr = products_arr.map(el => {
+                //     if (el.id == req_data.id) {
+                //         return req_data
+                //     }
+                //     return el
+                // });
+
+                let new_arr = products_arr.filter(el => el.id != req_data.id)
+
+                fs.writeFileSync("products.json", JSON.stringify({ products: new_arr }))
+            })
+            //fs.writeFileSync("file.txt", data)
+            req.on("end", function () {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end("success")
+            })
+        }
+
+
+
     } else if (req.url == "/users") {
         res.write("users list")
         res.end();
