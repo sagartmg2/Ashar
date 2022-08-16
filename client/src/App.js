667d6369f69f0056250ece8b1b46ecb7f6301314
Sagar <dev.sagartmg@gmail.com>
2022-08-16 17:07:56 +0545

    initiate ecommerce

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <Navbar />
+      </div>
+
+    </>
+  );
+}
+
+export default App;
